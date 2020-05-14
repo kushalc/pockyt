7699b799e7e8758cecf9efa164cdb250c25981ed
Kushal Chakrabarti <kushalc@talent.works>
2020-05-13 22:03:58 -0700

    Switched to naive bayes.

--- pockyt/topics.py
+++ pockyt/topics.py
@@ -8,20 +8,42 @@ import numpy as np
 import pandas as pd
 from bs4 import BeautifulSoup
 from sklearn.pipeline import make_pipeline
-from sklearn import compose as cp, dummy, feature_extraction as fe, impute, multioutput, neighbors, preprocessing as pp
+from sklearn import compose as cp, dummy, feature_extraction as fe, impute, multioutput, naive_bayes, neighbors, preprocessing as pp
 
 # FIXME: Remove me.
 from util.caching import cache_parquet_today, cache_today
 from util.performance import instrument_latency
 
+STOPWORDS = [
+    "a", "about", "above", "above", "across", "after", "afterwards", "again", "against", "all", "almost", "alone", "along", "already", "also",
+    "although","always","am","among", "amongst", "amoungst", "amount",  "an", "and", "another", "any","anyhow","anyone","anything","anyway", "anywhere", "are",
+    "around", "as",  "at", "back","be","became", "because","become","becomes", "becoming", "been", "before", "beforehand", "behind", "being", "below", "beside",
+    "besides", "between", "beyond", "bill", "both", "bottom","but", "by", "call", "can", "cannot", "cant", "co", "con", "could", "couldnt", "cry", "de", "describe",
+    "detail", "do", "done", "down", "due", "during", "each", "eg", "eight", "either", "eleven","else", "elsewhere", "empty", "enough", "etc", "even", "ever",
+    "every", "everyone", "everything", "everywhere", "except", "few", "fifteen", "fify", "fill", "find", "fire", "first", "five", "for", "former", "formerly",
+    "forty", "found", "four", "from", "front", "full", "further", "get", "give", "go", "had", "has", "hasnt", "have", "he", "hence", "her", "here", "hereafter",
+    "hereby", "herein", "hereupon", "hers", "herself", "him", "himself", "his", "how", "however", "hundred", "ie", "if", "in", "inc", "indeed", "interest",
+    "into", "is", "it", "its", "itself", "keep", "last", "latter", "latterly", "least", "less", "ltd", "made", "many", "may", "me", "meanwhile", "might", "mill",
+    "mine", "more", "moreover", "most", "mostly", "move", "much", "must", "my", "myself", "name", "namely", "neither", "never", "nevertheless", "next", "nine",
+    "no", "nobody", "none", "noone", "nor", "not", "nothing", "now", "nowhere", "of", "off", "often", "on", "once", "one", "only", "onto", "or", "other", "others",
+    "otherwise", "our", "ours", "ourselves", "out", "over", "own","part", "per", "perhaps", "please", "put", "rather", "re", "same", "see", "seem", "seemed",
+    "seeming", "seems", "serious", "several", "she", "should", "show", "side", "since", "sincere", "six", "sixty", "so", "some", "somehow", "someone", "something",
+    "sometime", "sometimes", "somewhere", "still", "such", "system", "take", "ten", "than", "that", "the", "their", "them", "themselves", "then", "thence", "there",
+    "thereafter", "thereby", "therefore", "therein", "thereupon", "these", "they", "thick", "thin", "third", "this", "those", "though", "three", "through",
+    "throughout", "thru", "thus", "to", "together", "too", "top", "toward", "towards", "twelve", "twenty", "two", "un", "under", "until", "up", "upon", "us",
+    "very", "via", "was", "we", "well", "were", "what", "whatever", "when", "whence", "whenever", "where", "whereafter", "whereas", "whereby", "wherein",
+    "whereupon", "wherever", "whether", "which", "while", "whither", "who", "whoever", "whole", "whom", "whose", "why", "will", "with", "within", "without",
+    "would", "yet", "you", "your", "yours", "yourself", "yourselves", "the"
+]
+
 class AutoTagger(multioutput.MultiOutputClassifier):
-    def __init__(self, featurizer=None, estimator=None):
+    def __init__(self, featurizer=None, estimator=None, proba_min=0.750):
         if featurizer is None:
             featurizer = self._build_featurizer()
         if estimator is None:
             estimator = self._build_estimator()
         super().__init__(estimator=estimator)
-        self.set_params(featurizer=featurizer)
+        self.set_params(featurizer=featurizer, proba_min=proba_min)
 
     # FIXME: Remove me.
     @instrument_latency
@@ -38,7 +60,9 @@ class AutoTagger(multioutput.MultiOutputClassifier):
     def predict(self, untagged_df):
         untagged_df = untagged_df[_get_ivars(untagged_df)]
         Xt = self.featurizer_.transform(untagged_df)
-        return super().predict(Xt)
+        Yt_proba = np.dstack(super().predict_proba(Xt))[:, 1, :]
+        Yt = (Yt_proba >= self.proba_min).astype(int)
+        return Yt
 
     def transform(self, untagged_df):
         # classification-based
@@ -87,9 +111,9 @@ class AutoTagger__Dummy(AutoTagger):
             ("title_bow", self._protect_nullable(fe.text.CountVectorizer()), "resolved_title"),
         ], remainder="drop")
 
-class AutoTagger__KNN(AutoTagger):
+class AutoTagger__NaiveBayes(AutoTagger):
     def _build_estimator(self):
-        return neighbors.KNeighborsClassifier()
+        return naive_bayes.ComplementNB()
 
     def _build_featurizer(self):
         def __binary_tfidf():
@@ -143,10 +167,10 @@ class AutoTagger__KNN(AutoTagger):
             # NOTE: Distracts from more effective content-based features. When used, need one-hot encoding
             # since ordinal encoding breaks KNN.
             # ("url_domain", self._protect_nullable(fe.text.CountVectorizer(min_df=1)), "resolved_domain"),
-            ("url_categories", self._protect_nullable(fe.text.CountVectorizer(min_df=5, binary=True)), "resolved_path"),
+            ("counts_title", fe.text.TfidfVectorizer(min_df=5, max_df=0.100, binary=True, stop_words=STOPWORDS), "combined_title"),
         ], remainder="drop")
 
-def build_auto_tagger(tagged_df, model_cls=AutoTagger__KNN):
+def build_auto_tagger(tagged_df, model_cls=AutoTagger__NaiveBayes):
     tagger = model_cls().fit(tagged_df[_get_ivars(tagged_df)], tagged_df["tags"])
     return tagger
 
@@ -156,7 +180,9 @@ def augment_dataset(saved_df):
         if pd.isnull(url):
             return pd.NA
         domain = urlparse(url).hostname
-        domain = ".".join(domain.rsplit(".")[-2:])
+
+        count = 2 if re.match(".*(com|org|net|edu|gov|co|io|google|biz|is|engineering)$", domain) else 3
+        domain = ".".join(domain.rsplit(".")[-count:])
         return domain
     saved_df["resolved_domain"] = saved_df["resolved_url"].apply(__extract_domain)
 
@@ -198,12 +224,16 @@ def augment_dataset(saved_df):
     # saved_df["text"] = saved_df["text"].str.strip().str.replace(r"\s{2,}", " ")
     # saved_df.loc[saved_df["text"].str.len() == 0, "text"] = pd.NA
 
+    saved_df["combined_title"] = saved_df[["resolved_title", "resolved_path"]].fillna("").apply(" ".join, axis=1)
+    # saved_df["combined_text"] = saved_df["text"].fillna("")
     return saved_df
 
 TAGGABLE_IVARS = [
     "resolved_title",
     "excerpt",
     "text",
+    "combined_title",
+    "combined_text",
     "resolved_url",
     "resolved_domain",
     "resolved_path",
